4d0d27e29ea859519f133b335cdbc29ad57c2c94
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-03-17 21:05:57 -0300

    Add SQL queries for requerimentos and cursos analysis

--- revisao_modelagem_dados/schema.sql
+++ revisao_modelagem_dados/schema.sql
@@ -368,3 +368,100 @@ GROUP BY
     usuario.cpf,
     aluno.matricula
 HAVING COUNT(requerimento.id) > 1;
+
+-- 15. 
+SELECT requerimento.id, 
+count(requerimento_id) as qtde 
+FROM requerimento 
+left join anexo 
+    on (requerimento.id = anexo.requerimento_id) 
+group by 
+    requerimento.id, requerimento_id;
+
+-- x. Quantidade de requerimentos por status.
+SELECT
+    status,
+    COUNT(status) AS qtde
+FROM requerimento
+GROUP BY
+    status;
+
+-- x. Tipos com mais de 1 ocorrência.
+SELECT
+    COUNT(status) AS qtd,
+    status
+FROM requerimento 
+GROUP BY
+    status
+HAVING COUNT(status) >= 1;
+
+-- x. Requerimentos por aluno.
+SELECT
+    aluno.matricula,
+    COUNT(requerimento.aluno_matricula)
+FROM requerimento
+RIGHT JOIN aluno
+    ON aluno.matricula = requerimento.aluno_matricula
+GROUP BY
+    aluno.matricula;
+
+-- x. Ano de abertura + contagem.
+-- (quantos requerimentos por ano)
+SELECT
+    EXTRACT(YEAR FROM data_hora_abertura) AS ano,
+    EXTRACT(MONTH FROM data_hora_abertura) AS mes,
+    COUNT(*) AS quantidade
+FROM requerimento
+GROUP BY 
+    ano,
+    mes;
+
+-- x. Mês atual.
+SELECT
+    EXTRACT(YEAR FROM data_hora_abertura) AS ano,
+    EXTRACT(MONTH FROM data_hora_abertura) AS mes,
+    CASE
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 1 THEN 'Janeiro'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 2 THEN 'Fevereiro'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 3 THEN 'Março'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 4 THEN 'Abril'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 5 THEN 'Maio'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 6 THEN 'Junho'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 7 THEN 'Julho'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 8 THEN 'Agosto'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 9 THEN 'Setembro'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 10 THEN 'Outubro'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 11 THEN 'Novembro'
+        WHEN EXTRACT(YEAR FROM data_hora_abertura) = 12 THEN 'Dezembro'
+    COUNT(*) AS quantidade
+FROM requerimento
+GROUP BY 
+    ano,
+    mes;
+
+-- x. Média duração cursos.
+SELECT 
+    AVG(duracao)::float 
+WHERE duracao > 
+(
+    SELECT 
+        CAST(AVG(duracao) as float) AS media_horas_duracao 
+    FROM curso
+)
+
+-- x. Cursos acima da média.
+SELECT * 
+FROM curso
+WHERE duracao > 
+(
+    SELECT 
+        CAST(AVG(duracao) as float) AS media_horas_duracao 
+    FROM curso
+)
+
+
+
+
+
+
+
