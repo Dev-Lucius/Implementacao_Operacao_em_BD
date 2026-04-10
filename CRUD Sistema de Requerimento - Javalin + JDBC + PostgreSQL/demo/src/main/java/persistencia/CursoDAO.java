2595aedca4f2b27caa6d8c0ab942ab9ecfea0663
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-04-10 07:37:35 -0300

    Refactor CursoDAO with additional CRUD operations
    Refactor CursoDAO to improve error handling and add methods for updating and deleting courses.

--- CRUD Sistema de Requerimento - Javalin + JDBC + PostgreSQL/demo/src/main/java/persistencia/CursoDAO.java
+++ CRUD Sistema de Requerimento - Javalin + JDBC + PostgreSQL/demo/src/main/java/persistencia/CursoDAO.java
@@ -1,85 +1,185 @@
-package persistencia;
+package  persistencia;
 
-// Importações Necessárias para Trabalhar com o Banco de Dados JDBC
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-// Estruturas de Dados
 import java.util.ArrayList;
 import java.util.List;
 
 import negocio.Curso;
 
-public class CursoDAO {
     /**
-     * Método resonsável por inserir um novo curso ao banco de dados
-     * @param curso --> Objeto contendo os dados que serão inseridos
+     * DAO --> Data Acess Object
+     * É a classe responsável por pelas operações de persistência da entidade no Banco de Dados
+     * 
+     * DAO PATTERN
+     * A camada DAO isola toda a lógica de acesso ao banco de dados.
+     * As demais camadas (negócio, apresentação) não sabem como os dados são armazenados.
+     * Elas apenas chamam os métodos de DAO !!
+     * 
+     * try - with - resources
+     * Garante que Connection, PreparedStatement e ResulSet sejam
+     * fechados automaticamente ao final do bloco, mesmo em casos de 
+     * exceção - evitando que resouce leak - vazamento de recursos
      * 
      */
+
+public class CursoDAO{
+
+    /**
+     * Método --> Insere um Novo Curso no Banco de dados !!
+     * 
+     * PreparedStatement
+     * Esse método usa "?" como placeholders em vez de concatenar em Strings, isso é útil pois:
+     *  - Eveita SQL Injection 
+     *  - Melhora a Performance do Algoritmo
+     *  - Permite o banco reutilizar o plano de execução
+     * 
+     * @param curso Objeto com dados a inserir
+     */
     public void inserirCurso(Curso curso){
-        // Comando SQL com parâmetros (?)
-        //  - Evita SQL Injection
-        //  - Melhora a Performance do código
+        if(curso == null){
+            throw new IllegalArgumentException("Curso Inválido: Não pode ser Nulo");
+        }
+        
         String sql = "INSERT INTO curso (nome, site, turno, duracao) VALUES (?, ?, ?, ?)";
+        
+        try(
+            Connection connection = new ConexaoPostgreSQL().getConexao();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+        ) {
+            stmt.setString(1, curso.getNome());
+            stmt.setString(2, curso.getSite());
+            // Turno é enum, não String
+            // Assim, é necessário converter para String para Salvar no Banco.
+            // name() --> retorna o nome da Constante
+            // toLowerCase() --> Converte os caracteres de maiúsculo para minúsculo 
+            stmt.setString(3, curso.getTurno().name().toLowerCase());
+            stmt.setInt(4, curso.getId());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao Inserir Curso: " + e.getMessage(), e);
+        }
+    }
+
+    /** 
+     * Método --> Retorna todos os cursos que foram cadastrados no curso
+     * @return Retorna a Lista de Cursos, ou uma lista vazia
+    */
+    public List<Curso> listarCursos(){
+        List<Curso> cursos = new ArrayList<>();
+
+        String sql = "SELECT * FROM cursos ORDER BY nome ASC";
 
         try(
-            // Abrindo o banco de daos
             Connection connection = new ConexaoPostgreSQL().getConexao();
-            // Preparando o comando SQL para execução
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+        ) {
+            while(rs.next()){
+                cursos.add(mapearCurso(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao Listar Curso: " + e.getMessage(), e);
+        }
+        
+        return cursos;
+    }
+
+    /**
+     * Método --> Usado para buscar um curso a partir do seu ID
+     * @param id ID do curso a ser buscado
+     * @return o Curso encontrado, ou null, se ele não existir
+     */
+    public Curso buscarPorId(int id){
+        String sql = "SELECT * FROM curso WHERE id = ?";
+
+        try(
+            Connection connection = new ConexaoPostgreSQL().getConexao();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+        ) {
+            stmt.setInt(1, id);
+
+            try(ResultSet rs = stmt.executeQuery()) {
+              if(rs.next()){
+                return mapearCurso(rs);
+              }  
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao Buscar Curso por ID: " + e.getMessage(), e);
+        }
+        return null; // Não Encontrado
+    }
+
+    public void atualizarCurso(Curso curso){
+        if(curso == null){
+            throw new IllegalArgumentException("Curso Inválido: Não pode ser nulo");
+        }
+
+        String sql = "UPDATE curso SET nome = ?, site = ?, turno = ?, duracao = ? WHERE id = ?";
+
+        try(
+            Connection connection = new ConexaoPostgreSQL().getConexao();
             PreparedStatement stmt = connection.prepareStatement(sql);
         ){
-            stmt.setString(1, curso.getNomeCurso());
-            stmt.setString(2, curso.getSiteCurso());
-            stmt.setString(3, curso.getTurnoCurso());
-            stmt.setString(4, curso.getDuracaoCurso());
+            stmt.setString(1, curso.getNome());
+            stmt.setString(2, curso.getSite());
+            stmt.setString(3, curso.getTurno().name().toLowerCase());
+            stmt.setInt(4, curso.getDuracao());
+            stmt.setInt(5, curso.getId());
 
-            // Executando o Comando Insert no Banco
             stmt.executeUpdate();
-        } catch(SQLException e){
-            /**
-             * EM CASO DE ERROS
-             * Encapsulamos a exceção em RunTimeException
-             * A fim de tratar o erro a partir de outras camadas
-             */
-            throw new RuntimeException("Erro ao Inserir curso", e);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao Atualizar Curso: " + e.getMessage(), e);
         }
     }
 
     /**
-     * Método Responsável por buscar todos os cursos do banco
-     * @return --> Ao final retorna a lista de Objetos
+     * Método --> Remove um Curso a partir do seu ID
+     * @param id
      */
-    public List<Curso> listarCursos(){
-
-        List<Curso> vetCurso = new ArrayList<>();
-        String sql = "SELECT * FROM curso ORDER BY ASC";
+    public void deletarCurso(int id){
+        String sql = "DELETE FROM curso WHERE id = ?";
 
         try (
             Connection connection = new ConexaoPostgreSQL().getConexao();
             PreparedStatement stmt = connection.prepareStatement(sql);
-
-            // Executa a Consulta e Obtêm o resultado
-            ResultSet rs = stmt.executeQuery();
         ){
-            while(rs.next()){
-                vetCurso.add(mapearCurso(rs));
-            }
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            
         } catch (SQLException e) {
-            throw new RuntimeException("Erro ao Listar Cursos", e);
+            throw new RuntimeException("Erro ao Deletar Curso: " + e.getMessage(), e);
         }
-        return vetCurso;
     }
 
-    private Curso mapearCurso(ResultSet rs) throws SQLException{
-        Curso curso = new Curso();
-        curso.setIdCurso(rs.getInt("id"));
-        curso.setNomeCurso(rs.getString("nome"));
-        curso.setSiteCurso(rs.getString("site"));
-        curso.setDuracaoCurso(rs.getString("duracao"));
-        curso.setTurnoCurso(rs.getString("turno"));
+    /**
+     * Método --> Mapeia uma Linha do ResultSet para um objeto Curso
+     * @param rs ResultSet posicionado na linha atual
+     * @return Objeto Curso Mapeado
+     * @throws SQLException Se ocorrer erro ao ler o ResultSet
+     */
+    public Curso mapearCurso(ResultSet rs) throws SQLException{
+        String nomeTurno = rs.getString("turno").toUpperCase();
 
-        return curso;
+        // Conceito - Enum.valueOf()
+        /**
+         * Converte a String do banco de volta para a constante Enum (Curso.Turno.NOTURNO)
+         * toUpperCase() é necessário pois os valores do banco estão em minúsculo e o
+         * enum em maiúsculo
+         */
+        Curso.Turno turno = Curso.Turno.valueOf(nomeTurno);
+
+        return new Curso(
+            rs.getString("nome"),
+            rs.getString("site"),
+            turno,
+            rs.getInt("duracao")
+        );
     }
 }
